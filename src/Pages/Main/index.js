ccc2dc9c015af6cb09f33bde3fad2be45554d669
Harry <yury_regis@hotmail.com>
2021-06-10 21:22:39 -0300

    Home Page v0.1
    Implementação de navegação dos pokemons consumindo api.

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,39 +1,98 @@
 import React, {useState, useEffect} from "react";
-import api from "../../api";
+import {api, fecthURL} from "../../api";
+import NavBar from "../../Components/Navbar";
+import NavButtons from "../../Components/NavButtons";
+import Tazo from "../../Components/Tazo";
 
+const URL = 'https://pokeapi.co/api/v2/pokemon';
 
 function Main() {
-    const [data, setData] = useState([]);
+    const [pokeData, setData] = useState([]);
     const [prevURL, setPrevURL] = useState('');
     const [nextURL, setNextURL] = useState('');
     const [loadingStatus, setLoadingStatus] = useState(true);
 
-    const URL = 'https://pokeapi.co/api/v2/pokemon';
 
     useEffect( () => {
       async function fetchData() {
-        let resposta = await api.get(URL);
-        console.log(resposta);
-        setNextURL(resposta.next);
-        setPrevURL(resposta.previous);
+        const resposta = await api.get(URL);
+        await loadingPokemon(resposta.data.results);
+        setNextURL(resposta.data.next);
+        setPrevURL(resposta.data.previous);
         setLoadingStatus(false);
       }
       fetchData();
     },[])
 
+    async function next() {
+      setLoadingStatus(true);
+      let newData = await api.get(nextURL);
+      await loadingPokemon(newData.data.results);
+      console.log("NEXT URL ->",nextURL);
+      setNextURL(newData.data.next);
+      setPrevURL(newData.data.previous);
+      setLoadingStatus(false);
+    }
+
+    async function prev() {
+      if (prevURL==='') return; 
+      setLoadingStatus(true);
+      let newData = await api.get(prevURL);
+      await loadingPokemon(newData.data.results);
+      setNextURL(newData.data.next);
+      setPrevURL(newData.data.previous);
+      setLoadingStatus(false);
+    }
+
+    async function loadingPokemon(data) {
+      let _pokemonData = await Promise.all(
+        data.map(
+          async pokemon => {
+            // console.log(pokemon.url);
+            let registroPokemon = await fecthURL(pokemon.url);
+            return registroPokemon;
+      }));
+      setData(_pokemonData);
+      console.log("_pokeData", _pokemonData);
+    }
+
     return (
-        <div style={{
-          display: "flex",  
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh"
-        }}>
-          {loadingStatus ? <h1>Carregando...</h1> : (
-            <h1>Lista carregada</h1>
-          )}  
+      <>
+        <NavBar />
+        <div style={{display:"flex", justifyContent:"center"}}>
+          <button style={{margin:"1vh", background:"orange", color:"white"}} onClick={prev}>Voltar</button>
+          <button style={{margin:"1vh", background:"orange", color:"white"}} onClick={next}>Próximo</button>
         </div>
-       
-      );
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            // // justifyContent: "center",
+            // // alignItems: "center",
+            // height: "100vh"
+          }}
+        >
+          {loadingStatus ? (
+            <h1>Carregando...</h1>
+          ) : (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                }}
+              >
+                {pokeData.map((pokemon, index) => {
+                  return <Tazo key={index} pokemon={pokemon.data} />;
+                })}
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    ); // Fim return
 }
 
 export default Main;
