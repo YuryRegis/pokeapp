f1a264c6a03da2181a0ec19aba57945ebc4b80fe
Harry <yury_regis@hotmail.com>
2021-06-10 21:25:54 -0300

    NavBar Component
    Barra de navegação adicionado com fundo laranja e Logo de teste

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import styled from "styled-components"
+
+
+const Logo = styled.img`
+    height: 10vh;
+`;
+
+const BarraNavegacao = styled.nav`
+    height: 10vh;
+    display: flex;
+    padding: 0 5vw;
+    justify-content: space-between;
+    background-color: orange;
+`;
+
+function NavBar() {
+    return (
+        <BarraNavegacao>
+            <Logo src="https://logosmarcas.net/wp-content/uploads/2020/05/Pokemon-Logo.png"/>
+        </BarraNavegacao>
+    )
+}
+
+
+export default NavBar
